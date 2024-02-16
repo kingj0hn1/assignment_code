d03bb117ea6ac7c90860ab8bfa87c119e66dd7e5
John <127602644+kingj0hn1@users.noreply.github.com>
2024-02-16 13:55:54 +0000

    Delete fizzbuzz.py

--- fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for number in range(0, 100):
-    if number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    elif number % 3 == 0 and number % 5 == 0:
-        print("Fizz_Buzz")
-    else:
-        print(number)
